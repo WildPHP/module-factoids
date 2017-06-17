b90f941142781c1382fe9358d73e101c17714c69
Yoshi2889 <rick.2889@gmail.com>
2017-06-17 21:50:37 +0200

    FactoidPool: Add proper copyright and getByKey, plus construct with Factoid as type
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/FactoidPool.php
+++ src/FactoidPool.php
@@ -1,18 +1,34 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: rick2
- * Date: 24-4-2017
- * Time: 13:09
+ * WildPHP - an advanced and easily extensible IRC bot written in PHP
+ * Copyright (C) 2017 WildPHP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace WildPHP\Modules\Factoids;
-
 
 use Collections\Collection;
 
 class FactoidPool extends Collection
 {
+	public function __construct()
+	{
+		parent::__construct(Factoid::class);
+	}
+
 	/**
 	 * @return array
 	 */
@@ -26,6 +42,19 @@ class FactoidPool extends Collection
 			$array[] = $factoid->toArray();
 
 		return $array;
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return false|Factoid
+	 */
+	public function findByKey(string $key)
+	{
+		return $this->find(function (Factoid $factoid) use ($key)
+		{
+			return $key == $factoid->getName();
+		});
 	}
 
 	/**
